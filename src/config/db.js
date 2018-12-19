a27449a6dd32f1cbadfc9b81fa117d9da5c9bca5
at-quando <quan.do@asiantech.vn>
2018-12-19 10:19:40 +0700

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,9 @@
 /** @type {{url:string}} */
 module.exports = {
-  url: undefined // must be loaded from env
+  db_name: undefined,
+  db_user: undefined,
+  db_password: undefined,
+  db_host: undefined,
+  db_dialect: undefined,
+  db_port: undefined
 };
