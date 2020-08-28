551092fa91a4e15b58d2a86fcafe480888905d43
quando1910 <dhquan1910@gmail.com>
2020-08-28 16:31:10 +0700

    [Refactor] New structure of expressjs

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,12 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
-// import morgan from 'morgan';
-// import compress from 'compression';
 import routes from '../routes/v1';
-// import { logs } from './vars';
-// import strategies from './passport';
-// import error from '../api/middlewares/error';
 
 /*
 * Express instance
@@ -33,14 +28,5 @@ app.use(cors());
 
 // mount api v1 routes
 app.use('/v1', routes);
-
-// // if error is not an instanceOf APIError, convert it.
-// app.use(error.converter);
-
-// // catch 404 and forward to error handler
-// app.use(error.notFound);
-
-// // error handler, send stacktrace only during development
-// app.use(error.handler);
 
 export default app;
