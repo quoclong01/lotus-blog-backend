cac26da4e25a48e53bcedef04601b500bca053cd
st-quando <quan.do@supremetech.vn>
2021-06-24 13:54:34 +0700

    fix config

--- db/config.js
+++ db/config.js
@@ -5,14 +5,22 @@ require('dotenv-safe').config({
   path: path.join(__dirname, '../.env')
 });
 
-module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  seederStorageTableName: 'Seeder',
-  autoMigrateOldSchema: true,
-  
-};
+let config = {};
+if (process.env.NODE_ENV === 'development') {
+  config = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'Seeder',
+    autoMigrateOldSchema: true,
+  };
+} else if (process.env.NODE_ENV === 'production') {
+  config = {
+    "use_env_variable": process.env.DATABASE_URL
+  };
+}
+
+module.exports = config;
