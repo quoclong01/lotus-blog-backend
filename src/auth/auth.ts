3647f95fb384dbc5bb30780d1f06c965d3361767
Nhi Nguyen Q. V <nhi.nguyen@supremetech.vn>
2021-07-05 21:20:09 +0700

    full flow sign in with google

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -5,46 +5,50 @@ import { RequestUser } from '../models/User';
 import { ProviderType } from '../lib/enum';
 
 passport.use(new GoogleStrategy({
-  clientID: '515502300212-s2ghn8cju5ro94s9v07a2rqkla65e65p.apps.googleusercontent.com',
-  clientSecret: '5dRXKgAgrYfq0mX5CfjOzpuv',
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: `${process.env.API_URL}/auths/google/callback`,
   passReqToCallback: true
 },
 async (request:any, accessToken: string, refreshToken: string, profile: any, done: any) => {
-  // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    console.log('profile', profile)
     const user = await User.findOne({
       where: { email: profile.email },
       include: { model: Auth, where: {providerType: ProviderType.GOOGLE} }
     });
 
     if (!user) {
-      const dataTemp: any = new RequestUser({
+      const userTemp = new User({
         email: profile.email,
         firstName: profile.family_name,
         lastName: profile.given_name,
         displayName: profile.displayName,
         picture: profile.picture,
+        isAdmin: false,
+        isActive: true,
+        followers: null,
+        followings: null,
+        phone: null,
+        gender: null,
+        dob: null,
         verifyAt: new Date()
       });
-      const userTemp = new User({ ...dataTemp });
       const user = await userTemp.save();
       const authTemp = new Auth({
+        accessToken,
+        refreshToken,
         providerType: ProviderType.GOOGLE,
-        accessToken: accessToken,
-        refreshToken: refreshToken,
         password: null,
         resetToken: null,
         userId: user.id
       });
       await authTemp.save();
+      return done(null, profile);
     }
-    console.log('user', user)
-    // console.log('refreshToken', refreshToken)
-    // console.log('profile', profile)
+
+    const authTemp = await Auth.findOne({ where: { userId: user.id, providerType: ProviderType.GOOGLE } });
+    await authTemp.update({ accessToken, refreshToken });
     return done(null, profile);
-  // });
-}
+  }
 ));
 
 passport.serializeUser((user, done) => {
