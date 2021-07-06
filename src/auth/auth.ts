4d7d9a13dba436fc23d0eca1fe98c2d374c5be84
Nhi Nguyen Q. V <nhi.nguyen@supremetech.vn>
2021-07-06 12:34:41 +0700

    add github oauth

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,6 @@
 import passport from 'passport';
-import { Strategy as GoogleStrategy} from 'passport-google-oauth2';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 import { User, Auth } from '../models';
 import { RequestUser } from '../models/User';
 import { ProviderType } from '../lib/enum';
@@ -12,8 +13,7 @@ passport.use(new GoogleStrategy({
 },
 async (request:any, accessToken: string, refreshToken: string, profile: any, done: any) => {
     const user = await User.findOne({
-      where: { email: profile.email },
-      include: { model: Auth, where: {providerType: ProviderType.GOOGLE} }
+      where: { email: profile.email }
     });
 
     if (!user) {
@@ -21,7 +21,6 @@ async (request:any, accessToken: string, refreshToken: string, profile: any, don
         email: profile.email,
         firstName: profile.family_name,
         lastName: profile.given_name,
-        displayName: profile.displayName,
         picture: profile.picture
       })
       const userTemp = new User({ ...dataTemp });
@@ -42,9 +41,22 @@ async (request:any, accessToken: string, refreshToken: string, profile: any, don
       });
     }
 
-    const authTemp = await Auth.findOne({ where: { userId: user.id, providerType: ProviderType.GOOGLE } });
-    await authTemp.update({ accessToken, refreshToken });
-    await user.update({ verifyAt: new Date() })
+    const authTemp = await Auth.findOne({
+      where: { userId: user.id, providerType: ProviderType.GOOGLE }
+    });
+    
+    if (!authTemp) {
+      const auth: any = {
+        providerType: ProviderType.GOOGLE,
+        accessToken,
+        refreshToken,
+        userId: user.id
+      };
+      await Auth.create(auth);
+    } else {
+      await authTemp.update({ accessToken, refreshToken });
+      await user.update({ verifyAt: new Date() });
+    }
     return done(null, {
       accessToken,
       providerType: ProviderType.GOOGLE,
@@ -52,6 +64,64 @@ async (request:any, accessToken: string, refreshToken: string, profile: any, don
     });
   }
 ));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: `${process.env.API_URL}/auths/github/callback`,
+  scope: ['user:email']
+},
+async (accessToken:string, refreshToken:string, profile:any, done:any) => {
+  const email = profile.emails.length > 0 && profile.emails[0].value || undefined;
+  const picture = profile.photos.length > 0 && profile.photos[0].value || undefined;
+  const user = await User.findOne({
+    where: { email }
+  });
+  if (!user) {
+    const dataTemp: any = new RequestUser({
+      email,
+      picture
+    });
+    const userTemp = new User({ ...dataTemp });
+    const user = await userTemp.save();
+    const authTemp = new Auth({
+      accessToken,
+      refreshToken,
+      providerType: ProviderType.GITHUB,
+      password: null,
+      resetToken: null,
+      userId: user.id
+    });
+    await authTemp.save();
+    return done(null, {
+      accessToken,
+      providerType: ProviderType.GITHUB,
+      isNewUser: true
+    });
+  }
+
+  const authTemp = await Auth.findOne({
+    where: { userId: user.id, providerType: ProviderType.GITHUB }
+  });
+
+  if (!authTemp) {
+    const auth: any = {
+      providerType: ProviderType.GITHUB,
+      accessToken,
+      refreshToken,
+      userId: user.id
+    };
+    await Auth.create(auth);
+  } else {
+    await authTemp.update({ accessToken, refreshToken });
+    await user.update({ verifyAt: new Date() });
+  }
+  return done(null, {
+    accessToken,
+    providerType: ProviderType.GITHUB,
+    isNewUser: false
+  });
+}));
 
 passport.serializeUser((user, done) => {
   done(null, user);
--- src/routes/v1/auth.routes.ts
+++ src/routes/v1/auth.routes.ts
@@ -9,12 +9,30 @@ router.get('/google', (req, res, next) => {
 
   const authenticator = passport.authenticate('google', { scope: ['email', 'profile'], state });
   authenticator(req, res, next);
-})
+});
 
 router.get('/google/callback', passport.authenticate('google',
   { failureRedirect: '/failure' }
 ), (req, res, next) => {
-  console.log('req', req.user);
+  const { accessToken, isNewUser, providerType}: any = req.user;
+  const redirectTo = req.query['state'];
+  if (typeof redirectTo === 'string') {
+    return res.redirect(`${redirectTo}?accessToken=${accessToken}&isNewUser=${isNewUser}&providerType=${providerType}`)
+  }
+  res.redirect('/')
+});
+
+router.get('/github', (req, res, next) => {
+  const { redirectTo } = req.query;
+  const state = redirectTo ? redirectTo.toString() : undefined;
+
+  const authenticator = passport.authenticate('github', { state });
+  authenticator(req, res, next);
+});
+
+router.get('/github/callback', passport.authenticate('github',
+  { failureRedirect: '/failure' }
+), (req, res, next) => {
   const { accessToken, isNewUser, providerType}: any = req.user;
   const redirectTo = req.query['state'];
   if (typeof redirectTo === 'string') {
